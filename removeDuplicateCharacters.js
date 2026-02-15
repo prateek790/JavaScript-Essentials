ce531de143e86591bc867385ea2fa04d32019344
Prateek Gupta <guptaprateek790@gmail.com>
2026-02-15 23:57:05 +0530

    Remove DuplicateCharactersticks

---
+++ removeDuplicateCharacters.js
@@ -0,0 +1,14 @@
+let word = "Helllooo";
+removeDuplicate
+function removeDuplicate() {
+    let seen = {}
+    let result = "";
+    for (char of str) {
+        if(!seen[char]){
+            result +=char;
+            seen[char] = true
+        } 
+
+    }
+}
+console.log(result);
